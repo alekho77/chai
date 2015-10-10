b09460a9e9bcb94e1a3857ddb6ee46d71728ae5c
Aleksey Khozin <ahozin@hotmail.com>
2015-10-10 21:37:39 +0300

    The Packages project draft.

---
+++ src/Packages/pkginstall.py
@@ -0,0 +1,10 @@
+﻿import sys, os.path
+
+repodir = os.path.abspath(sys.argv[1])
+print(repodir)
+sys.path.append(repodir)
+
+import pkgsys.packages
+
+#if sys.argv[2] == "env":
+#    pkg.packages.setup()
