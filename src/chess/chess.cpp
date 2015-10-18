11af2f3d6132fb95b2f9bfdb919cfe64e97e399c
Aleksey Khozin <ahozin@hotmail.com>
2015-10-18 12:44:07 +0300

    Was added a draft of chess application.

---
+++ src/chess/chess.cpp
@@ -0,0 +1,12 @@
+#include "chess.h"
+
+chess::chess(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+}
+
+chess::~chess()
+{
+
+}
---
+++ src/chess/chess.h
@@ -0,0 +1,19 @@
+#ifndef CHESS_H
+#define CHESS_H
+
+#include <QtWidgets/QMainWindow>
+#include "ui_chess.h"
+
+class chess : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	chess(QWidget *parent = 0);
+	~chess();
+
+private:
+	Ui::chessClass ui;
+};
+
+#endif // CHESS_H
