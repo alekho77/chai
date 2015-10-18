b50027196e5e5bb2ed47bfc8396f05f570b27bf5
Aleksey Khozin <ahozin@hotmail.com>
2015-10-18 14:31:10 +0300

    Was added copying Qt bins into OutDir.

--- src/Packages/pkginstall.py
+++ src/Packages/pkginstall.py
@@ -1,4 +1,19 @@
-﻿import sys, os.path
+﻿import sys, os.path, shutil
 
 outdir = os.path.abspath(sys.argv[1])
-print (outdir)
+variant = sys.argv[2]
+print ("Install packages into %s with %s variant" % (outdir, variant))
+
+qtdir = "C:\Qt"
+qtmudules = ["Core", "Gui", "Widgets"]
+for m in qtmudules:
+    name = "Qt5" + m + ("d" if variant.lower()=="debug" else "")
+    for ext in ("dll", "pdb"):
+        extname = name + "." + ext
+        fullname = os.path.join(qtdir, "bin", extname)
+        if not os.path.exists(fullname):
+            sys.exit("Module %s not found" % fullname)
+        if not os.path.exists(os.path.join(outdir, extname)):
+            print("Copying %s" % extname)
+            shutil.copy(fullname, outdir)
+ 
