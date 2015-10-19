d9ef70027cb7211c4163e6ef1d0297c1c7bbb791
Aleksey Khozin <ahozin@hotmail.com>
2015-10-19 12:51:49 +0300

    Was implemented backlighting chessboard position.

--- src/chess/chessboard.cpp
+++ src/chess/chessboard.cpp
@@ -2,9 +2,13 @@
 #include <QPainter>
 #include <QPaintEvent>
 #include <QResizeEvent>
+#include <QMouseEvent>
 
 Chessboard::Chessboard(QWidget *parent)
   : QWidget(parent)
+  , cellLight(255, 206, 158)
+  , cellDark(209, 139, 71)
+  , hotFile(QChar::Null), hotRank(QChar::Null)
 {
   ui.setupUi(this);
 }
@@ -16,43 +20,50 @@ Chessboard::~Chessboard()
 
 void Chessboard::createChessboard(int size)
 {
-  const QColor cblight(255, 206, 158);
-  const QColor cbdark(209, 139, 71);
-
   const int cellsize = size / 10;
   const int startcorner = (size - cellsize * 8) / 2;
   const int centersize = (size - cellsize * 8 + 3 * cellsize * 8) / 3;
-  const int bandsize = (size - centersize) / 2;
 
-  rectStartCell = QRect(startcorner, startcorner, cellsize, cellsize);
+  startCell = QRect(startcorner, startcorner, cellsize, cellsize);
 
   imgBoard.reset();
   imgBoard.reset(new QImage(size, size, QImage::Format_RGB32));
-  imgBoard->fill(cbdark);
+  imgBoard->fill(cellDark);
   QPainter painter(&*imgBoard);
-  painter.fillRect((size - centersize) / 2, (size - centersize) / 2, centersize, centersize, cblight);
-  QRect cellrect = rectStartCell;
-  painter.setPen(cblight);
+  painter.fillRect((size - centersize) / 2, (size - centersize) / 2, centersize, centersize, cellLight);
+  QRect cellrect = startCell;
+  for (int i = 0; i < 8; i++)
+    for (int j = (i % 2 + 1) % 2; j < 8; j += 2)
+    {
+      cellrect.moveTo(startcorner + j * cellsize, startcorner + i * cellsize);
+      painter.fillRect(cellrect, cellDark);
+    }
+}
+
+void Chessboard::drawChessboardLabels(QPainter& painter)
+{
+  const int bandsize = (imgBoard->width() - 8 * startCell.width()) / 3;
+  painter.setPen(cellLight);
   QFont font("Arial Black");
-  font.setPixelSize(2 * cellsize / 3);
+  font.setPixelSize(2 * startCell.width() / 3);
   painter.setFont(font);
   QFontMetrics fm = painter.fontMetrics();
   for (int i = 0; i < 8; i++)
   {
-    QString row = QString::number(8 - i);
-    QRect symrect = fm.tightBoundingRect(row);
-    painter.drawText((bandsize - symrect.width()) / 2 - symrect.left(), startcorner + (i + 1) * cellsize - (cellsize - symrect.height()) / 2, row);
-    for (int j = (i % 2 + 1) % 2; j < 8; j += 2)
-    {
-      cellrect.moveTo(startcorner + j * cellsize, startcorner + i * cellsize);
-      painter.fillRect(cellrect, cbdark);
-    }
-  }
-  for (int i = 0; i < 8; i++)
-  {
-    QString row = QString('a' + i);
-    QRect symrect = fm.tightBoundingRect(row);
-    painter.drawText(startcorner + i * cellsize + (cellsize - symrect.width()) / 2 - symrect.left(), size - (bandsize - symrect.height()) / 2, row);
+    const QChar r('0' + 8 - i);
+    const QChar f('a' + i);
+    const QRect rrect = fm.tightBoundingRect(r);
+    const QRect frect = fm.tightBoundingRect(f);
+    if (hotRank == r)
+      painter.setPen(Qt::white);
+    else
+      painter.setPen(cellLight);
+    painter.drawText((bandsize - rrect.width()) / 2 - rrect.left(), startCell.top() + (i + 1) * startCell.height() - (startCell.height() - rrect.height()) / 2, r);
+    if (hotFile == f)
+      painter.setPen(Qt::white);
+    else
+      painter.setPen(cellLight);
+    painter.drawText(startCell.left() + i * startCell.width() + (startCell.width() - frect.width()) / 2 - frect.left(), imgBoard->height() - (bandsize - frect.height()) / 2, f);
   }
 }
 
@@ -66,7 +77,33 @@ void Chessboard::paintEvent(QPaintEvent * event)
 {
   QPainter painter(this);
   painter.drawImage(0, 0, *imgBoard);
-  //QRect r = rect();
-  //r.moveTo(0, 0);
-  //painter.fillRect(event->rect(), Qt::red);
+  drawChessboardLabels(painter);
+}
+
+void Chessboard::leaveEvent(QEvent * event)
+{
+  hotFile = hotRank = QChar::Null;
+  repaint();
+  QWidget::leaveEvent(event);
+}
+
+void Chessboard::mouseMoveEvent(QMouseEvent * event)
+{
+  if (imgBoard)
+  {
+    if (event->x() >= startCell.left() && event->x() < (startCell.left() + 8 * startCell.width())
+      && event->y() >= startCell.top() && event->y() < (startCell.top() + 8 * startCell.height()))
+    {
+      int x = (event->x() - startCell.left()) / startCell.width();
+      Q_ASSERT(x >= 0 && x < 8);
+      hotFile = QChar('a' + x);
+      int y = (event->y() - startCell.top()) / startCell.height();
+      Q_ASSERT(y >= 0 && y < 8);
+      hotRank = QChar('0' + 8 - y);
+    }
+    else
+      hotFile = hotRank = QChar::Null;
+    repaint();
+  }
+  QWidget::mouseMoveEvent(event);
 }
--- src/chess/chessboard.h
+++ src/chess/chessboard.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QSharedPointer>
+#include <QMap>
 #include "ui_chessboard.h"
 
 class Chessboard : public QWidget
@@ -16,13 +17,24 @@ public:
 protected:
   void resizeEvent(QResizeEvent * event) override;
   void paintEvent(QPaintEvent * event) override;
+  void leaveEvent(QEvent * event) override;
+  void mouseMoveEvent(QMouseEvent * event) override;
 
 private:
   void createChessboard(int size);
+  void drawChessboardLabels(QPainter& painter);
 
   Ui::chessboardClass ui;
+  
+  const QColor cellLight;
+  const QColor cellDark;
+
   QSharedPointer<QImage> imgBoard;
-  QRect rectStartCell;
+  QRect startCell;
+  QChar hotRank; // Rank - A row of the chessboard.In algebraic notation, ranks are numbered 1–8 starting from White's side of the board
+  QChar hotFile; // File - a column of the chessboard. A specific file are named using its position in a–h.
+  typedef QMap< QString, QSharedPointer<QImage> > ChessPiecesMap;
+  //ChessPiecesMap
 };
 
 #endif // CHESSBOARD_H
