3067012f30be2ac2a5951c070dab3d7011b00a87
Aleksey Khozin <ahozin@hotmail.com>
2015-10-20 22:16:54 +0300

    Added the first sketches of the interface IChessMachine.

--- src/chessmachine/chessmachine.h
+++ src/chessmachine/chessmachine.h
@@ -8,36 +8,38 @@
 
 namespace Chai {
   namespace Chess {
-    enum class Set : int { white, black };
-    enum class Type : int { pawn = 1, knight = 3, bishop = 4, rook = 5, queen = 9, king = 1000 };
+    //enum class Set : int { white, black };
+    enum class Type : int { bad = 0, pawn = 1, knight = 3, bishop = 4, rook = 5, queen = 9, king = 1000 };
 
-    
-
-    class IPiece {
-    public:
-      virtual Set GetSet() const = 0;
-      virtual Type GetType() const = 0;
-      virtual char GetFile() const = 0;
-      virtual char GetRank() const = 0;
-      virtual ~IPiece() {}
+    struct Postion
+    {
+      char file; // A column of the chessboard. A specific file are named using its position in 'a'–'h'.
+      char rank; // A row of the chessboard. In algebraic notation, ranks are numbered '1'–'8' starting from White's side of the board.
     };
 
+    struct Field
+    {
+      bool threat;
+      Postion postion;
+    };
+
+    struct PieceState
+    {
+      Type type;
+      Postion position;
+      Field moves[27];
+    };
+    
     struct Snapshot
     {
-      const IPiece* whitePieces[16];
-      const IPiece* blackPieces[16];
-    };
-
-    class IPosition {
-    public:
-      virtual const Snapshot GetSnapshot() const = 0;
-      virtual ~IPosition() {}
+      const PieceState whitePieces[16];
+      const PieceState blackPieces[16];
     };
 
     class IChessMachine {
     public:
       virtual void Start() = 0;
-      virtual const IPosition& GetPostion() const = 0;
+      virtual void GetSnapshot(Snapshot& snapshot) const = 0;
       virtual ~IChessMachine() {}
     };
 
