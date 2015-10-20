e710eabb98e53fefab6145ade3ba65481b9d9c0d
Aleksey Khozin <ahozin@hotmail.com>
2015-10-20 23:19:21 +0300

    Added ChessMachine Unit Test project.

---
+++ src/ChessMachineTest/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifdef _WIN32
+    #include "targetver.h"
+#endif
+
+#include <stdio.h>
+
+
+// TODO: reference additional headers your program requires here
+
+//Adding required boost header
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
--- src/Packages/pkginstall.py
+++ src/Packages/pkginstall.py
@@ -2,9 +2,10 @@
 
 outdir = os.path.abspath(sys.argv[1])
 variant = sys.argv[2]
+toolset = "vc" + sys.argv[3][1:]
 print ("Install packages into %s with %s variant" % (outdir, variant))
 
-qtdir = "C:\Qt"
+qtdir = "C:/Qt"
 qtmudules = ["Core", "Gui", "Widgets", "Svg"]
 for m in qtmudules:
     name = "Qt5" + m + ("d" if variant.lower()=="debug" else "")
@@ -16,4 +17,18 @@ for m in qtmudules:
         if not os.path.exists(os.path.join(outdir, extname)):
             print("Copying %s" % extname)
             shutil.copy(fullname, outdir)
- 
+
+boost_root = "C:/Boost"
+boost_ver = "1.59"
+boost_modules = ["unit_test_framework"]
+for m in boost_modules:
+    name = "boost_" + m + "-" + toolset + "-mt-" + ("gd-" if variant.lower()=="debug" else "") + boost_ver.replace('.', '_')
+    for ext in (["dll"]):
+        extname = name + "." + ext
+        fullname = os.path.join(boost_root, "lib", extname)
+        if not os.path.exists(fullname):
+            sys.exit("Module %s not found" % fullname)
+        if not os.path.exists(os.path.join(outdir, extname)):
+            print("Copying %s" % extname)
+            shutil.copy(fullname, outdir)
+        
