0f5041da48638907e995e349ef7de0b769c65723
Aleksey Khozin <ahozin@hotmail.com>
2015-10-24 00:49:39 +0300

    Added friendly macros. Removed Boost example.

--- src/chessmachine/chessmachine.h
+++ src/chessmachine/chessmachine.h
@@ -6,18 +6,22 @@
 #define CHESSMACHINE_API __declspec(dllimport)
 #endif
 
+#define CHESSPOS(name) const Postion (##name) = { (#name)[0], (#name)[1] }
+#define CHESSRANK(R) CHESSPOS(a##R); CHESSPOS(b##R); CHESSPOS(c##R); CHESSPOS(d##R); CHESSPOS(e##R); CHESSPOS(f##R); CHESSPOS(g##R); CHESSPOS(h##R)
+#define CHESSBOARD CHESSRANK(1); CHESSRANK(2); CHESSRANK(3); CHESSRANK(4); CHESSRANK(5); CHESSRANK(6); CHESSRANK(7); CHESSRANK(8)
+
+#define BADPOS Postion({0,0})
+
 namespace Chai {
   namespace Chess {
-    //enum class Set : int { white, black };
+    enum class Set : int { white, black };
     enum class Type : int { bad = 0, pawn = 1, knight = 3, bishop = 4, rook = 5, queen = 9, king = 1000 };
 
     struct Postion
     {
-      Postion() : file(0), rank(0) {}
-      Postion(char f, char r) : file(f), rank(r) {}
-      Postion(int hex) : file(), rank() {}
       char file; // A column of the chessboard. A specific file are named using its position in 'a'–'h'.
       char rank; // A row of the chessboard. In algebraic notation, ranks are numbered '1'–'8' starting from White's side of the board.
+      bool operator == (const Postion& other) const { return this->file == other.file && this->rank == other.rank; }
     };
 
     struct Snapshot
