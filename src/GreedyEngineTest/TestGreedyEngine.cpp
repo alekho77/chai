61b76ab07cd0333d7c9afee90b5ee18f5817dde6
Aleksey Khozin <ahozin@hotmail.com>
2015-11-14 22:29:11 +0300

    Stub of unit-test to the new library.

---
+++ src/GreedyEngineTest/TestGreedyEngine.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+
+BOOST_AUTO_TEST_SUITE( GreedyEngineTest )
+
+BOOST_AUTO_TEST_CASE(ConstructorTest)
+{
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ src/GreedyEngineTest/stdafx.h
@@ -0,0 +1,18 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifdef _WIN32
+    #include "targetver.h"
+#endif
+
+#include <stdio.h>
+
+
+// TODO: reference additional headers your program requires here
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
