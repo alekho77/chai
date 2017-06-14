8257ebacccd1bc95b6c922b57a4f0042cfeb46a2
Aleksey Khozin <ahozin@hotmail.com>
2017-06-14 23:53:33 +0300

    Issue #10: Qt has been switched on 5.9 with static link.

--- src/Packages/pkginstall.py
+++ src/Packages/pkginstall.py
@@ -5,18 +5,18 @@ variant = sys.argv[2]
 toolset = "vc" + sys.argv[3][1:]
 print ("Install packages into %s with %s variant" % (outdir, variant))
 
-qtdir = "C:/Qt"
-qtmudules = ["Core", "Gui", "Widgets", "Svg"]
-for m in qtmudules:
-    name = "Qt5" + m + ("d" if variant.lower()=="debug" else "")
-    for ext in ("dll", "pdb"):
-        extname = name + "." + ext
-        fullname = os.path.join(qtdir, "bin", extname)
-        if not os.path.exists(fullname):
-            sys.exit("Module %s not found" % fullname)
-        if not os.path.exists(os.path.join(outdir, extname)):
-            print("Copying %s" % extname)
-            shutil.copy(fullname, outdir)
+#qtdir = "C:/Qt"
+#qtmudules = ["Core", "Gui", "Widgets", "Svg"]
+#for m in qtmudules:
+#    name = "Qt5" + m + ("d" if variant.lower()=="debug" else "")
+#    for ext in ("dll", "pdb"):
+#        extname = name + "." + ext
+#        fullname = os.path.join(qtdir, "bin", extname)
+#        if not os.path.exists(fullname):
+#            sys.exit("Module %s not found" % fullname)
+#        if not os.path.exists(os.path.join(outdir, extname)):
+#            print("Copying %s" % extname)
+#            shutil.copy(fullname, outdir)
 
 #boost_root = "C:/Boost"
 #boost_ver = "1.59"
